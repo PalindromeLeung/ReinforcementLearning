8130af904f845ace3a93521f6cdd8cee98e81fcc
Hannah•Leung <hannah.leung@email.virginia.edu>
2018-04-09 22:54:50 -0400

    Update the Monte Carlo Exploring starts method. .

--- codes/hw4/MCES.py
+++ codes/hw4/MCES.py
@@ -16,28 +16,38 @@ def MCES(get_episode,initial_Q,initial_policy,gamma,alpha,num_episodes=1e4):
     iteration = 0
     
     while iteration < num_episodes:
-        iteration += 1                                        
 
-        initialState = np.random.randint(num_states)
-        initialAction = np.random.randint(num_actions)
-
-        for ep in range(num_episodes):
-            states,actions,rewards = get_episode(policy, initialState, initialAction) # generate an episode
-            G_total = 0
-            for s,state in enumerate(states):
-                first_occr = next(i for i,x in enumerate(states) if states[i] == state )
-                G = sum([ x * (gamma ** i) for i,x in enumerate(rewards[first_occr:])])
-                G_total += G
-                N_sa[state,actions[s]] += 1.0
-
-                if alpha ==0:
-                    Q[state,actions[s]]  += ( G_total - Q[state,actions[s]] )/N_sa[state,actions[s]] 
-
-                else:
-                    Q[state,actions[s]]  += alpha *(G_total - Q[state,actions[s]] )
+        """
+        Your code
+        """   
+       
         
+        for ep in range(num_episodes):
+            
+            iteration += 1   
+            
+            initialState = np.random.randint(num_states)
+            initialAction = np.random.randint(num_actions)
+#            initialAction = np.random.binomial(1,policy[initialState,1])
+                   
+            states,actions,rewards = get_episode(policy,initialState,initialAction) 
+
+            G = 0
+            for i,crnState in enumerate(states): 
+                frstOcc = next(s for s,x in enumerate(states) if crnState == states[s])
+                Gtmp = sum([x * (gamma ** r) for r,x in enumerate(rewards[frstOcc:])])
+                G += Gtmp
+                N_sa[crnState,actions[i]] += 1.0
+                if alpha == 0:
+                    Q[crnState,actions[i]] += (G - Q[crnState,actions[i]]) / N_sa[crnState,actions[i]]
+                else:
+                    Q[crnState,actions[i]] += alpha * (G - Q[crnState,actions[i]])
+          
             for state in states:
-                bestAction = np.argmax(Q[state,:])
-                policy[state] = Q[state,bestAction]
+                actIdx = np.argmax(Q[state,:])
+                policy[state,:] = 0
+                policy[state,actIdx] = 1
+            
+  
     return Q , policy
 
