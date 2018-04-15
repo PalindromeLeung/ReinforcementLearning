5ed7eb2eddd2db7ec3e30536f00e5ea7ed8640e8
Hannah•Leung <hannah.leung@email.virginia.edu>
2018-04-15 19:47:10 -0400

    Update Sarsa Lambda.

--- codes/hw4/sarsa_lambda.py
+++ codes/hw4/sarsa_lambda.py
@@ -28,7 +28,47 @@ def sarsa_lambda(initial_Q,initial_state,transition,
        """
         Your code
         """
-            
+        # epsilon greedy
+        uniformScl = epsilon/ num_actions
+        greedyScl = (1 - epsilon) + epsilon /num_actions
+
+        crnState = initial_state
+        # set up eligibility trace
+        ElgTrace = np.copy(initial_Q)
+
+        cnt = 0
+        imdRewards = 0
+        while True: 
+            cnt +=1 
+            nxtState, imdReward, terminal = transition(crnState,crnAction)
+            imdRewards += imdReward
+            # if it reaches terminal state then exploration
+            if terminal:
+                break   
+
+            # Take action A, observe R,S'
+            # Choose A' from S' using policy derived from Q(epsilon-greedy)
+
+            nxtActPrb = uniformScl * np.ones(num_actions,dtype = float)
+            nxtActPrb[np.argmax(Q[nxtState])] = greedyScl
+            nxtAction = np.random.choice(num_actions, p = nxtActPrb)
+
+            # delta = TD error
+            delta = imdReward + gamma*Q[nxtState,nxtAction] - Q[crnState,crnAction]
+
+            # update eligibility Trace 
+            ElgTrace[crnState,crnAction] += 1
+
+            for i in range(num_states):
+                for j in range(num_actions):
+                    Q[i,j] += Q[i,j] + alpha*delta*ElgTrace[i,j]
+                    ElgTrace[i,j] = gamma*lambda_*ElgTrace[i,j]
+
+            crnState = nxtState
+            crnAction = nxtAction
+        rewards[ep] += imdRewards
+        steps[ep] += cnt
+        
     return Q,  steps, rewards
         
     
