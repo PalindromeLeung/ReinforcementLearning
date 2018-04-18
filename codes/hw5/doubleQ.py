2a596ac7cd49802709f8e64a9316727bafd3751c
Hannah•Leung <hannah.leung@email.virginia.edu>
2018-04-18 12:24:32 -0400

    Add hw5

--- codes/hw5/doubleQ.py
+++ codes/hw5/doubleQ.py
@@ -4,9 +4,40 @@ def doubleQ(initial_Q1,initial_Q2,initial_state,transition,
            num_episodes,gamma, alpha, epsilon=0.1):
     #This function implements double Q-learning. It returns Q1, Q2 and their sum Q
     
-    """
-    Your code
-    """
-    
-    return Q1, Q2, Q #,  steps#, rewards
+    Q1 = np.copy(initial_Q1)
+    Q2 = np.copy(initial_Q2)
+    num_states, num_actions = Q1.shape
+    Q = Q1 + Q2
+
+    for ep in range(num_episodes):
+        crnState = initial_state
+
+        cnt = 0
+        imdRewards = 0.0
+
+        while True:
+            cnt +=1 
+
+            uniformScl = epsilon / num_actions
+            greedyScl = (1 - epsilon) + epsilon / num_actions
+            actionPrb = uniformScl * np.ones(num_actions,dtype= float)
+            actionPrb[np.argmax(Q[crnState,:])] = greedyScl
+            crnAction = np.random.choice(num_actions,p = actionPrb)
+            
+            
+            nxtState,imdReward, terminal = transition(crnState, crnAction)
+
+            if terminal:
+                break
+
+            if np.random.random() > 0.5:  # representaion of 0.5 probability
+                Q1[crnState,crnAction] += alpha * (imdReward + gamma *(Q2[nxtState,np.argmax(Q1[nxtState,:])]) - Q1[crnState,crnAction] )
+            
+            else:
+                Q2[crnState,crnAction] += alpha * (imdReward + gamma *(Q1[nxtState,np.argmax(Q2[nxtState,:])]) - Q2[crnState,crnAction] )
+
+            Q = Q1 + Q2
+            crnState = nxtState
+
+    return Q1, Q2, Q 
            
--- codes/hw5/q_learn.py
+++ codes/hw5/q_learn.py
@@ -16,9 +16,43 @@ def q_learn(initial_Q,initial_state,transition,
     -epsilon: exploration rate as in epsilon-greedy policy
     
     """    
-    
-    """ 
-    Your code
-    """
-            
+    Q = np.copy(initial_Q)
+    num_states, num_actions = Q.shape
+
+    steps = np.zeros(num_episodes, dtype = int)
+    rewards  = np.zeros(num_episodes,dtype = float)
+
+
+    for ep in range(num_episodes):
+
+        crnState = int(initial_state)
+
+        cnt = 0
+        imdRewards = 0.0
+
+        while True:
+            cnt += 1
+            if np.random.binomial(1, epsilon) == 1:
+                crnAction =  np.random.choice([i for i in range(num_actions)])
+            else: 
+                values_ = Q[crnState]
+                crnAction = np.random.choice([act for act, vl in enumerate(values_) if vl == np.max(values_)])
+
+            nxtState, imdReward, terminal = transition(crnState,crnAction)
+            if terminal:
+                break
+
+            imdRewards += imdReward
+
+            # Take action A, observe R,S'
+            # Choose A' from S' using policy derived from Q(epsilon-greedy)
+
+            TD_target = imdReward + gamma *  max(Q[nxtState]) 
+            Q[crnState][crnAction] += alpha * (TD_target - Q[crnState][crnAction])
+
+            crnState = nxtState
+
+        rewards[ep] += imdRewards
+        steps[ep] += cnt
+
     return Q,  steps, rewards
